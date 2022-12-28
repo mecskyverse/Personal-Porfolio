b5fc02bf943ca9b156eaba194848bde34ac8d51f
Aakash <vermaaakash1000@gmail.com>
2022-12-28 17:59:42 +0530

    Initial configuration added

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.scss'
+import { About, Footer, Header, Skills, Work, Testimonial } from './container/index'
+import { Navbar } from './components/index'
+function App() {
+
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+
+    </div>
+  )
+}
+
+export default App

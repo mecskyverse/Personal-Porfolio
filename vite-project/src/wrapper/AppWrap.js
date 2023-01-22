04b97fba60776d1395d7101626a887505df8e05e
Aakash <vermaaakash1000@gmail.com>
2023-01-22 19:08:06 +0530

    Higher components initialized

---
+++ vite-project/src/wrapper/AppWrap.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {NavigationDots, SocialMedia} from '../components' 
+const AppWrap = (Component, idName, classNames) => function HOC{
+  return (
+    <div>AppWrap</div>
+  )
+}
+
+export default AppWrap

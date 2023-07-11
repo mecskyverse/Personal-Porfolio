4fbd769077a3dae1fc074ea9358fcc27561df46c
Aakash <vermaaakash1000@gmail.com>
2023-07-11 16:40:25 +0530

    Wrap component added for header

--- vite-project/src/container/Header/Header.jsx
+++ vite-project/src/container/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import { images } from '../../constants'
 import './Header.scss'
-
+import { AppWrap } from '../../wrapper'
 const scaleVariants = {
     whileInView: {
         scale: [0, 1],
@@ -15,7 +15,7 @@ const scaleVariants = {
 }
 const Header = () => {
     return (
-        <div id="home" className='app__header app__flex'>
+        <div className='app__header app__flex'>
             <motion.div
                 whileInView={{ x: [-200, 0], opacity: [0, 1] }}
                 transition={{ duration: 0.5 }}
@@ -64,4 +64,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default AppWrap(Header, 'home');

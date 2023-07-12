7968cbb802c3cd2fdebb155eb939fb6f5b57c72c
Aakash <vermaaakash1000@gmail.com>
2023-07-12 21:01:04 +0530

    App Wrap updated

--- vite-project/src/wrapper/AppWrap.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {NavigationDots, SocialMedia} from '../components' 
-const AppWrap = (Component, idName, classNames) => function HOC{
-  return (
-    <div>AppWrap</div>
-  )
-}
-
-export default AppWrap

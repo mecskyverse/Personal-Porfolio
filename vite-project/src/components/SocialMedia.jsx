46224f18414629ce9ef5f5d42b7ebdff91cba601
Aakash <vermaaakash1000@gmail.com>
2023-08-15 22:11:20 +0530

    Icons Link added

--- vite-project/src/components/SocialMedia.jsx
+++ vite-project/src/components/SocialMedia.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import { BsTwitter, BsInstagram } from 'react-icons/bs'
-import { FaFacebookF } from 'react-icons/fa'
+import { BsTwitter, BsInstagram, BsGithub } from 'react-icons/bs'
 const SocialMedia = () => {
+    const openTwitterAccount = () => {
+        window.open('https://x.com/mecskyverse', '_blank');
+    };
+    const twitterAccountUrl = 'https://x.com/mecskyverse';
+    const instagramAccountUrl = 'https://instagram.com/mecskyverse'
+    const githubAccountUrl = 'https://github.com/mecskyverse'
     return (
-        <div className='app__social'>
-            <div><BsTwitter /></div>
-            <div><FaFacebookF /></div>
-            <div><BsInstagram /></div>
 
-        </div>
+        <div className='app__social'>
+            {/* <div onClick={openTwitterAccount}></div> */}
+            <a href={githubAccountUrl} target="_blank" rel="noopener noreferrer"><div><BsGithub /></div></a>
+            <a href={twitterAccountUrl} target="_blank" rel="noopener noreferrer"><div><BsTwitter /></div></a>
+            <a href={instagramAccountUrl} target="_blank" rel="noopener noreferrer"><div><BsInstagram /></div></a>
+        </div >
     )
 }
 
